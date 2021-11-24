d901f5e63d5076c0705ec05abe0e175858a9c0ba
BADCoCoBunny <gracebradlol@gmail.com>
2021-11-23 22:51:50 -0500

    Version inicial, multiplicacion de dos numeros enteros.

---
+++ multiplicacion.py
@@ -0,0 +1,9 @@
+print("--- Multiplicación de enteros ---")
+n1=int(input(">>Ingrese el Primer Numero: "))
+n2=int(input(">>Ingrese el Segundo Numero: "))
+
+multiplicacion = n1+n2;
+
+print(">>La suma es : " + str(multiplicacion))
+
+print("Felicidades Sumo los dos numeros correctamente :D")
